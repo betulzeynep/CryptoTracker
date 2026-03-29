5c71516eaeeeb825bc10c34d02e68bf45e05a080
LogicAndLines <betulzeynepseyis@gmail.com>
2026-03-29 04:41:43 +0200

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Zeynep Turnalı on 29.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
