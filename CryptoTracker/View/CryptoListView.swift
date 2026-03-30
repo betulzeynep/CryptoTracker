0973417a6a6e2cccf460f62790d934565b2258bb
LogicAndLines <betulzeynepseyis@gmail.com>
2026-03-30 22:19:03 +0200

    default CryptoListView created

---
+++ CryptoTracker/View/CryptoListView.swift
@@ -0,0 +1,49 @@
+//
+//  CryptoListView.swift
+//  CryptoTracker
+//
+//  Created by Zeynep Turnalı on 30.03.2026.
+//
+import SwiftUI
+
+struct CryptoListView: View {
+    @State private var viewModel = CryptoListViewModel()
+    
+    var body: some View {
+        NavigationStack {
+            content
+                .navigationTitle("Crypto Tracker")
+                .task {
+                    await viewModel.fetchCryptos()
+                }
+        }
+    }
+    
+    @ViewBuilder
+    private var content: some View {
+        if viewModel.isLoading {
+            ProgressView("Loading cryptocurrencies...")
+        } else if let errorMessage = viewModel.errorMessage {
+            ContentUnavailableView {
+                Label("Error", systemImage: "exclamationmark.triangle")
+            } description: {
+                Text(errorMessage)
+            } actions: {
+                Button("Retry") {
+                    Task {
+                        await viewModel.fetchCryptos()
+                    }
+                }
+            }
+        } else {
+            List(viewModel.cryptos, id: \.id) { crypto in
+               // CryptoRow(crypto: crypto)
+                Text(crypto.name)
+            }
+        }
+    }
+}
+
+#Preview {
+    CryptoListView()
+}
