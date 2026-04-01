cc49a121da1c3d54254c1b38ddf1a05d38652dfc
LogicAndLines <betulzeynepseyis@gmail.com>
2026-04-01 02:00:43 +0200

    created CoinDetailView

---
+++ CryptoTracker/View/CoinDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  CoinDetailView.swift
+//  CryptoTracker
+//
+//  Created by Zeynep Turnalı on 31.03.2026.
+//
+import SwiftUI
+
+struct CoinDetailView: View {
+    let coinId: String
+    @State private var viewModel = CoinDetailViewModel()
+    
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    CoinDetailView(coinId: "bitcoin")
+}
